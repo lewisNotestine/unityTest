a75a43f3aa21c1f0c82c758bb0d01baa6bfe130c
Lewis S Notestine <lewis.notestine@gmail.com>
2014-04-17 21:40:54 -0700

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <inheritdoc/>
+	public void FixedUpdate() {
+		float moveHoriz = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+		rigidbody.velocity = movement;
+	}
+}
