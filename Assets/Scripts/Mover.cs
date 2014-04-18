3431db0b8ddb5ed431c3aaf820424cabf7fb4c97
Lewis S Notestine <lewis.notestine@gmail.com>
2014-04-18 02:03:34 -0700

    added assets and scripts for simple creation of a LASERS, which are translated vertically up when created, created a prefab from the Bolt asset so it can be reproduced

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	private const float DEFAULT_SPEED = 10;
+
+	[SerializeField] private float speed;
+
+	public float Speed{get{return speed;} set{speed = value;}}
+
+	public Mover() {
+		Speed = DEFAULT_SPEED;
+	}
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = transform.forward * Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
