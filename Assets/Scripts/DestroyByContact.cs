96911767e45de3afa016feb3301fc34f5f273df1
Lewis S Notestine <lewis.notestine@gmail.com>
2014-04-19 15:39:39 -0700

    created asteroid hazard, per tutorial #10

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Destroys the owning object, as well as another object when the owning object's collider collides with it.
+/// </summary>
+public class DestroyByContact : MonoBehaviour {
+	private void OnTriggerEnter(Collider otherCollider) {
+		if(otherCollider.tag == "Boundary") {
+			return;
+		}
+
+		Destroy(otherCollider.gameObject);
+		Destroy(gameObject);
+	}	
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Makes an object's RigidBody tumble with randomly generated angular velocity. 
+/// </summary>
+public class RandomRotator : MonoBehaviour {
+
+	private const float DEFAULT_TUMBLE = 1;
+
+	[SerializeField] private float tumble;
+
+	public float Tumble {get{return tumble;} set{tumble = value;}}
+
+	public RandomRotator() {
+		Tumble = DEFAULT_TUMBLE;
+	}
+
+	private void Start() {
+		rigidbody.angularVelocity = Random.insideUnitSphere * Tumble;
+	}
+}
