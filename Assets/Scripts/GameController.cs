92ff6587c09cf204861e5bf963991b3fcd33dd6b
Lewis S Notestine <lewis.notestine@gmail.com>
2014-04-19 19:07:54 -0700

    following tut 13, added repetition for hazard wave spawning, garbage-collection for explosions

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Main GameController class for controlling operations between player, hazards, enemies, etc.
+/// </summary>
 public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 	
 	private void Start() {
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 	}
 
-	private void SpawnWaves() {
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x) , spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate(hazard, spawnPosition, spawnRotation);
+	private IEnumerator SpawnWaves() {
+		yield return new WaitForSeconds(startWait);
+		while (true) {
+			
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x) , spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Debug.Log ("instantiating Hazard: " + i);
+				Instantiate(hazard, spawnPosition, spawnRotation);
+	
+				//Co-routines, operate asynchronously
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
